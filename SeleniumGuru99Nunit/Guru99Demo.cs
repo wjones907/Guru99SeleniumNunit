4d02fbd5e2519c821c33ba4354f91f1c48fb2740
wjones907 <wjones907@outlook.com>
2021-12-30 08:24:48 -0800

    upload files from -og directory
    upload remaining files for this solution

---
+++ SeleniumGuru99Nunit/Guru99Demo.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guru99Demo
+{
+    class Guru99Demo
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("E:\\Users\\Bigwen\\Documents\\My Projects\\github_repos\\Guru99SeleniumNunit\\packages\\Selenium.WebDriver.ChromeDriver.91.0.4472.10100\\driver\\win32");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
